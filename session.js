904a07cd327ecc74d1cda10589f07ee82c859a2f
atharaka25-rgb <atharaka25@gmail.com>
2026-05-25 22:53:21 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1779729440836_4awk1p8r2",
 PORT: process.env.PORT || "8000"
 };
